15d2eb79d1f50be5c1cf7a9d4683adc644742ba2
Fabio Souza <fabio.souza011@hotmail.com>
2024-03-27 23:06:41 -0300

    File Manipulation and Functions Dictionary

--- Dicionarios/Funcoes.py
+++ Dicionarios/Funcoes.py
@@ -9,3 +9,9 @@ def inserir(dicionario):
     dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
                                                    input("Digite a última data de acesso: "),
                                                    input("Qual a última estação acessada: ").upper()]
+    salvar(dicionario)
+
+def salvar(dicionario):
+    with open("bd.txt", "a") as arquivo:
+        for chave, valor in dicionario.items():
+            arquivo.write(chave + ":" + str(valor))
---
+++ Manipulacao_Arquivos/textos.py
@@ -0,0 +1,6 @@
+texto = "Amelie Lens e Charlotte de Witte"
+print(texto[0:11])
+print(texto[12:13])
+print(texto[14:])
+print(texto[-18:])
+print(texto[::-1])
